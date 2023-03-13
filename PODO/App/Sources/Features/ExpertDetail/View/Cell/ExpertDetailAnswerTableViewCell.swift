2f0cc40a6335fc4d191302a4c845aa1ce56524a2
Ethan <nicebuvr@naver.com>
2023-03-14 00:56:18 +0900

    Add ExpertDetail View

---
+++ PODO/App/Sources/Features/ExpertDetail/View/Cell/ExpertDetailAnswerTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExpertDetailAnswerTableViewCell.swift
+//  PODO
+//
+//  Created by Ethan on 2023/02/24.
+//
+
+import UIKit
+
+final class ExpertDetailAnswerTableViewCell: UITableViewCell {
+
+}
---
+++ PODO/App/Sources/Features/ExpertDetail/View/Cell/ExpertDetailQuestionTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExpertDetailQuestionTableViewCell.swift
+//  PODO
+//
+//  Created by Ethan on 2023/02/24.
+//
+
+import UIKit
+
+final class ExpertDetailQuestionTableViewCell: UITableViewCell {
+
+}
