6da683319c3eef3312244261d5527e323ef99bf3
Ethan <nicebuvr@naver.com>
2023-03-17 13:34:05 +0900

    Add ToastManager

---
+++ PODO/App/Sources/Manager/ToastManager.swift
@@ -0,0 +1,22 @@
+//
+//  ToastManager.swift
+//  PODO
+//
+//  Created by Yun on 2023/03/17.
+//
+
+import Foundation
+
+final class ToastManager {
+
+    static func showToast(type: ToastType) {
+
+    }
+}
+
+extension ToastManager {
+
+    enum ToastType {
+        case error(Error)
+    }
+}
